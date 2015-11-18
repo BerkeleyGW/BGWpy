3b00fabaf0677a043fd8213486cafd4370c10d48
Gabriel Antonius <gabriel.antonius@gmail.com>
2015-11-18 13:51:01 -0800

    Shit just got real.

--- Examples/t24-Absorption.py
+++ Examples/t24-Absorption.py
@@ -44,10 +44,10 @@ task = AbsorptionTask(
     wfn_co_fname='14-Wfn_co/wfn_co.cplx',
     wfn_fi_fname='15-Wfn_fi/wfn_fi.cplx',
     wfnq_fi_fname='16-Wfnq_fi/wfnq_fi.cplx',
-    eps0mat_fname='21-Epsilon/eps0mat',
-    epsmat_fname='21-Epsilon/epsmat',
-    bsedmat_fname='23-Kernel/bsedmat',
-    bsexmat_fname='23-Kernel/bsexmat',
+    eps0mat_fname='21-Epsilon/eps0mat',  # Change to 21-Epsilon/eps0mat.h5 if BGW is compiled with HDF5
+    epsmat_fname='21-Epsilon/epsmat',  # Change to 21-Epsilon/epsmat.h5 if BGW is compiled with HDF5
+    bsedmat_fname='23-Kernel/bsedmat',  # Change to 23-Kernel/bsedmat.h5 if BGW is compiled with HDF5
+    bsexmat_fname='23-Kernel/bsexmat',  # Change to 23-Kernel/bsexmat.h5 if BGW is compiled with HDF5
     sigma_fname='22-Sigma/sigma_hp.log',
     eqp_fname='22-Sigma/eqp1.dat',
 
