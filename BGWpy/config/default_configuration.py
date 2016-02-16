9113335aecb1e08cf3355e009a562c9c435ba424
Gabriel Antonius <gabriel.antonius@gmail.com>
2016-02-16 09:58:59 -0800

    Fixed user configuration.

--- BGWpy/config/default_configuration.py
+++ BGWpy/config/default_configuration.py
@@ -3,11 +3,13 @@
 # Do not modify these by hand. Use the user_configuration file instead.
 
 default_mpi = dict(
-            nproc = 1,
-            nproc_per_node = 1,
-            mpirun = 'mpirun',
-            nproc_flag = '-n',
-            nproc_per_node_flag = '--npernode',
+    mpirun = 'mpirun',
+    nproc = 1,
+    nproc_flag = '-n',
+    nproc_per_node = 1,
+    nproc_per_node_flag = '--npernode',
+    nodes = None,
+    nodes_flag = None,
     )
 
 default_runscript = dict(
